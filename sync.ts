407901f8b9bc0984740f043226e2531014d0d61a
Leandro Roldan <leandrom.roldan@gmail.com>
2023-01-10 13:16:24 -0300

    add: comment on sync file

--- sync.ts
+++ sync.ts
@@ -3,5 +3,5 @@ import "./models";
 
 sequelize.sync({ alter: true }).then((res) => console.log(res));
 
-// use this for complete base reset
+// use this for complete base reset with command yarn sync
 // sequelize.sync({ force: true }).then((res) => console.log(res));
